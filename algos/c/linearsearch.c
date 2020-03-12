1adf4bf3e7500b8ec0e7cd97bb0562590e2cf399
Sree Charitha <38373549+RSreeCharitha@users.noreply.github.com>
2020-03-11 23:33:40 -0700

    Update linearsearch.c

--- algos/c/linearsearch.c
+++ algos/c/linearsearch.c
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+int main(){
+	int n, flag = 0;
+	scanf("%d", &n);
+	int a[n];
+	int i;
+	for ( i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	int key;
+	scanf("%d", &key);
+	for ( i=0;i<n;i++){
+		if(key==a[i]){
+			flag = 1;
+			scanf("Found at position %d.",i);
+			break;
+		}
+	}
+	if(flag==0){
+		printf("Key not found");
+	}
+}
