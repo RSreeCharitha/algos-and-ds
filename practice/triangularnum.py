776ac24c954dce02367e2a8e4d65f594d1b39503
charitha <reddycharitha.1454@gmail.com>
2020-09-28 05:16:10 +0530

    tri series

--- practice/triangularnum.py
+++ practice/triangularnum.py
@@ -1,5 +1,15 @@
-def trinum(n):
-    start = 1
+def trinum1(n):
+    start = [1]
+    for i in range(1,n+1):
+        print(sum(start),end=" ")
+        start.append(i+1)
 
+def trinum2(n):
+    for i in range(0,n):
+        su=0
+        for j in range(i+1):
+            su+=j+1
+        print(su,end=" ")
 
 n = int(input())
+trinum2(n)
